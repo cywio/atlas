c314ef51e5235d6629f0c9fb4378366611d7d3c0
Chris Walsh <christopher@drumshift.com>
2021-09-29 21:04:38 -0700

    Add database expose and unexpose endpoint

---
+++ pages/api/databases/[id]/expose.ts
@@ -0,0 +1,36 @@
+import ssh from '../../../../lib/server/ssh'
+import log from '../../../../lib/server/log'
+import prisma from '../../../../lib/server/db'
+import getSession from '../../../../lib/server/session'
+
+export default async function (req, res) {
+	try {
+		let { id } = req.query
+		let { id: accountId, admin } = await getSession(req, res)
+
+		let database = await prisma.databases.findUnique({
+			where: { id },
+			include: { accounts: true },
+		})
+
+		if (!database) return res.status(404).send()
+		if (!admin && database.owner !== accountId) return res.status(403).send()
+
+		if (req.method === 'POST') {
+			let port = await ssh(`dokku ${database.type}:expose ${database.id}`)
+			await prisma.databases.update({ where: { id }, data: { port: String(port).split('->')[3] } })
+			await log(req, accountId, `Database connection was exposed for ${database.name}`)
+			res.status(204).send()
+		} else if (req.method === 'DELETE') {
+			await prisma.databases.update({ where: { id }, data: { port: null } })
+			await ssh(`dokku ${database.type}:unexpose ${database.id}`)
+			await log(req, accountId, `Database connection was unexposed for ${database.name}`)
+			res.status(204).send()
+		} else {
+			res.status(405).send()
+		}
+	} catch (e) {
+		if (typeof e == 'undefined') return e
+		res.status(500).send()
+	}
+}
