74dd8cd8a808d09c825b5c45703183255e0ba466
Chris Walsh <christopher@drumshift.com>
2021-09-29 22:41:10 -0700

    Add auth page and add global css to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { Toaster } from 'react-hot-toast'
 import 'tailwindcss/tailwind.css'
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Toaster />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
---
+++ pages/auth.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { Input, Button } from '@components'
+import { useApi } from '@hooks'
+import toast from 'react-hot-toast'
+import cookie from 'js-cookie'
+
+export default function Auth() {
+	const [form, setForm] = useState({ email: null, password: null })
+	const [loading, setLoading] = useState(false)
+
+	async function login() {
+		setLoading(true)
+		try {
+			let token = await useApi('/api/auth', 'POST', form)
+			cookie.set('session', token)
+			window.location.href = '/'
+		} catch {
+			setLoading(false)
+			toast.error('Incorrect credentials')
+		}
+	}
+
+	return (
+		<div className='grid place-items-center min-h-screen'>
+			<div className='w-96 p-7'>
+				<Input type='email' disabled={loading} label='Email' onChange={({ target }) => setForm({ ...form, email: target.value })} />
+				<Input
+					type='password'
+					disabled={loading}
+					label='Password'
+					onChange={({ target }) => setForm({ ...form, password: target.value })}
+				/>
+				<div className='mt-6'>
+					<Button loading={loading} disabled={loading || !form.email || !form.password} onClick={() => login()}>
+						Login
+					</Button>
+				</div>
+			</div>
+		</div>
+	)
+}
