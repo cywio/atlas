f7e6aaa715d4931e4bbb4bb38219568b31767241
Chris Walsh <christopher@drumshift.com>
2021-09-29 22:40:49 -0700

    Add api hook

---
+++ lib/hooks/useAPI.tsx
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import cookie from 'js-cookie'
+
+export async function useApi(url, method = 'GET', body = {}) {
+	let { data } = await axios(url, {
+		//@ts-ignore
+		method,
+		headers: {
+			authorization: cookie.get('session') ? `Bearer ${cookie.get('session')}` : undefined,
+		},
+		...(method !== 'GET' && { data: body }),
+	})
+	return data
+}
