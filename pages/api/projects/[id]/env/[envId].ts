303b3373085d624b0f30569be318e514acddc956
Chris Walsh <christopher@drumshift.com>
2021-09-29 21:31:52 -0700

    Add environment variable create, delete endpoints

---
+++ pages/api/projects/[id]/env/[envId].ts
@@ -0,0 +1,41 @@
+import ssh from '../../../../../lib/server/ssh'
+import log from '../../../../../lib/server/log'
+import prisma from '../../../../../lib/server/db'
+import getSession from '../../../../../lib/server/session'
+
+export default async function (req, res) {
+	try {
+		let { envId, id: projectId, restart } = req.query
+		let { id: accountId, admin } = await getSession(req, res)
+
+		restart = Boolean(restart)
+
+		let project = await prisma.projects.findUnique({
+			where: { id: projectId },
+			include: { accounts: true },
+		})
+
+		if (!project) return res.status(404).send()
+		if (!admin && project.owner !== accountId) return res.status(403).send()
+
+		let envVar = await prisma.environment_variables.findFirst({
+			where: { id: envId, project: projectId },
+		})
+
+		if (!envVar) return res.status(404).send()
+
+		if (req.method === 'GET') {
+			res.json(envVar)
+		} else if (req.method === 'DELETE') {
+			res.status(202).send()
+			await prisma.environment_variables.delete({ where: { id: envId } })
+			await ssh('dokku', ['config:unset', project.id, envVar.key, !restart ? '--no-restart' : undefined])
+			await log(req, accountId, `Environment variable ${envVar.key} was deleted`)
+		} else {
+			res.status(405).send()
+		}
+	} catch (e) {
+		if (typeof e == 'undefined') return e
+		res.status(500).send()
+	}
+}
---
+++ pages/api/projects/[id]/env/index.ts
@@ -0,0 +1,59 @@
+import ssh from '../../../../../lib/server/ssh'
+import prisma from '../../../../../lib/server/db'
+import getSession from '../../../../../lib/server/session'
+import log from '../../../../../lib/server/log'
+
+export default async function (req, res) {
+	try {
+		let { id, restart } = req.query
+		let { id: accountId, admin } = await getSession(req, res)
+
+		restart = Boolean(restart)
+
+		let project = await prisma.projects.findUnique({
+			where: { id },
+			include: { accounts: true },
+		})
+
+		if (!project) return res.status(404).send()
+		if (!admin && project.owner !== accountId) return res.status(403).send()
+
+		if (req.method === 'GET') {
+			let envVars = await prisma.environment_variables.findMany({
+				where: { project: project.id },
+			})
+			res.json(envVars)
+		} else if (req.method === 'POST') {
+			let { key, value } = req.body
+			let envVars = await prisma.environment_variables.create({
+				data: {
+					projects: {
+						connect: {
+							id: project.id,
+						},
+					},
+					key,
+					value,
+					accounts: {
+						connect: {
+							id: accountId,
+						},
+					},
+				},
+			})
+			res.status(202).json(envVars)
+			await log(req, accountId, `Environment variable ${key} was created`)
+			await ssh('dokku', ['config:set', project.id, `${key}=${value}`, !restart ? '--no-restart' : undefined])
+		} else if (req.method === 'DELETE') {
+			res.status(202).send()
+			await prisma.environment_variables.deleteMany({ where: { project: id } })
+			await ssh('dokku', ['config:clear', project.id, !restart ? '--no-restart' : undefined])
+			await log(req, accountId, `All environment variables for ${project.name} were cleared`)
+		} else {
+			res.status(405).send()
+		}
+	} catch (e) {
+		if (typeof e == 'undefined') return e
+		res.status(500).send()
+	}
+}
