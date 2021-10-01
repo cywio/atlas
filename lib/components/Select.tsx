255508fb033e0c2c979d1de2bf8dfcddad286087
Chris Walsh <christopher@drumshift.com>
2021-10-01 16:23:50 -0700

    Add select component

---
+++ lib/components/Select.tsx
@@ -0,0 +1,15 @@
+export function Select(props) {
+	return (
+		<div>
+			<label htmlFor='price' className='block text-sm font-medium text-gray-700'>
+				{props.label}
+			</label>
+			<select
+				{...props}
+				className='mt-1 relative rounded-md shadow-sm p-2 border block w-full sm:text-sm border-gray-300 rounded-md mb-3'
+			>
+				{props.children}
+			</select>
+		</div>
+	)
+}
