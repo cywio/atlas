a0ecefcedb7d1ccbbe5c6597b54d765d14e6a58c
Chris Walsh <christopher@drumshift.com>
2021-10-06 18:51:11 -0700

    Add nothing available component

---
+++ lib/components/Nothing.tsx
@@ -0,0 +1,10 @@
+export function Nothing(props) {
+	return (
+		<div {...props} className={`w-full h-64 border rounded-lg grid place-items-center`}>
+			<div className='flex flex-col justify-center opacity-40 p-12'>
+				<img src='/icons/alert.svg' className='w-8 m-auto mb-3' />
+				<p>{props.text}</p>
+			</div>
+		</div>
+	)
+}
