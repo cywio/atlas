de562e7cd750fb7aca20d16ea998b1f9c3e0410f
Chris Walsh <christopher@drumshift.com>
2021-10-06 18:51:14 -0700

    Change useApi hook to not throw error

--- lib/hooks/useAPI.tsx
+++ lib/hooks/useAPI.tsx
@@ -5,6 +5,7 @@ export async function useApi(url, method = 'GET', body = {}) {
 	let { data } = await axios(url, {
 		//@ts-ignore
 		method,
+		validateStatus: () => true,
 		headers: {
 			authorization: cookie.get('session') ? `Bearer ${cookie.get('session')}` : undefined,
 		},
