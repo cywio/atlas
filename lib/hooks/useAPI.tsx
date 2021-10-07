3ca31194c28e0482a590cefaeaec143afb6c232a
Chris Walsh <christopher@drumshift.com>
2021-10-06 19:35:33 -0700

    Remove useApi validate to fix conflict with toast

--- lib/hooks/useAPI.tsx
+++ lib/hooks/useAPI.tsx
@@ -5,7 +5,6 @@ export async function useApi(url, method = 'GET', body = {}) {
 	let { data } = await axios(url, {
 		//@ts-ignore
 		method,
-		validateStatus: () => true,
 		headers: {
 			authorization: cookie.get('session') ? `Bearer ${cookie.get('session')}` : undefined,
 		},
