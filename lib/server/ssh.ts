6b114827005fd79885d043779f478c6817376a08
Chris Walsh <christopher@drumshift.com>
2021-10-12 15:52:54 -0700

    Fix keys path

--- lib/server/ssh.ts
+++ lib/server/ssh.ts
@@ -7,7 +7,7 @@ export default async function ssh(cmd, args = [], clientOnly = false) {
 		let client = await ssh.connect({
 			host: process.env.SERVER_IP,
 			username: cmd === 'dokku' ? 'dokku' : process.env.DOCKER_USER,
-			privateKey: cmd === 'dokku' ? fs.readFileSync('keys/dokku', 'utf8') : fs.readFileSync('keys/docker', 'utf8'),
+			privateKey: cmd === 'dokku' ? fs.readFileSync('/keys/dokku', 'utf8') : fs.readFileSync('/keys/docker', 'utf8'),
 		})
 		if (clientOnly) return client
 		if (cmd === 'dokku') {
