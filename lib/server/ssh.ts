4249168f6eabb3ae52984e61c9e814f831860ac7
Chris Walsh <christopher@drumshift.com>
2021-10-12 14:58:36 -0700

    Move to file based SSH keys

--- lib/server/ssh.ts
+++ lib/server/ssh.ts
@@ -1,21 +1,19 @@
 import { NodeSSH } from 'node-ssh'
+import fs from 'fs'
 
 export default async function ssh(cmd, args = [], clientOnly = false) {
 	const ssh = new NodeSSH()
 	try {
-		let client =
-			cmd === 'dokku'
-				? await ssh.connect({
-						host: process.env.SERVER_IP,
-						username: 'dokku',
-						privateKey: process.env.DOKKU_SSH_KEY,
-				  })
-				: await ssh.connect({
-						host: process.env.SERVER_IP,
-						username: process.env.DOCKER_USER,
-						privateKey: process.env.DOCKER_SSH_KEY,
-				  })
+		let client = await ssh.connect({
+			host: process.env.SERVER_IP,
+			username: cmd === 'dokku' ? 'dokku' : process.env.DOCKER_USER,
+			privateKey: cmd === 'dokku' ? fs.readFileSync('keys/dokku', 'utf8') : fs.readFileSync('keys/docker', 'utf8'),
+		})
 		if (clientOnly) return client
+		if (cmd === 'dokku') {
+			cmd = args[0]
+			args.shift()
+		}
 		return await client.exec(cmd, args, {
 			onStdout: (chunk) => chunk.toString('utf8'),
 			onStderr: (chunk) => chunk.toString('utf8'),
