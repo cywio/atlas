35c08c71d5dfa470738f2266545d5193b6355459
Chris Walsh <christopher@drumshift.com>
2021-10-11 21:05:10 -0700

    Add class names to nothing component

--- lib/components/Nothing.tsx
+++ lib/components/Nothing.tsx
@@ -1,6 +1,6 @@
 export function Nothing(props) {
 	return (
-		<div {...props} className={`w-full h-64 border rounded-lg grid place-items-center`}>
+		<div {...props} className={`w-full h-64 border rounded-lg grid place-items-center ${props.className}`}>
 			<div className='flex flex-col justify-center opacity-40 p-12'>
 				<img src='/icons/alert.svg' className='w-8 m-auto mb-3' />
 				<p>{props.text}</p>
