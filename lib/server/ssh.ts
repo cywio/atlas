f26f1aa9d95cd28aa6240fbbe135071e6678defe
Chris Walsh <christopher@drumshift.com>
2021-10-12 01:59:49 -0700

    Add ssh key and docker user support to install/ssh

--- lib/server/ssh.ts
+++ lib/server/ssh.ts
@@ -3,14 +3,18 @@ import { NodeSSH } from 'node-ssh'
 export default async function ssh(cmd, args = [], clientOnly = false) {
 	const ssh = new NodeSSH()
 	try {
-		/**
-		 * Todo: Move to ssh keys later
-		 */
-		let client = await ssh.connect({
-			host: process.env.SERVER_IP,
-			username: process.env.SERVER_USER,
-			password: process.env.SERVER_PASSWORD,
-		})
+		let client =
+			cmd === 'dokku'
+				? await ssh.connect({
+						host: process.env.SERVER_IP,
+						username: 'dokku',
+						password: process.env.DOKKU_SSH_KEY,
+				  })
+				: await ssh.connect({
+						host: process.env.SERVER_IP,
+						username: process.env.DOCKER_USER,
+						password: process.env.DOCKER_SSH_KEY,
+				  })
 		if (clientOnly) return client
 		return await client.exec(cmd, args, {
 			onStdout: (chunk) => chunk.toString('utf8'),
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ HOST_IP=$(curl -sS https://ipinfo.io/ip)
 CURRENT_USER=$(whoami)
 CURRENT_VERSION=$(curl -sS https://api.github.com/repos/dokku/dokku/releases/latest | grep -oP '(?<="tag_name": ")[^"]*')
 SETUP_KEY=$(openssl rand -hex 16)
+DOCKER_USER="a$(openssl rand -hex 8)"
 REPO_URL=$1
 
 #Check if source repo argument exists
@@ -48,8 +49,39 @@ echo '-----> Installing dashboard'
 dokku apps:create admin
 dokku postgres:create admin
 dokku postgres:link admin admin
-dokku config:set admin SERVER_IP=$HOST_IP SETUP_KEY=$SETUP_KEY SECRET=$(openssl rand -hex 64)
+dokku config:set admin SERVER_IP=$HOST_IP SETUP_KEY=$SETUP_KEY SECRET=$(openssl rand -hex 64) DOCKER_USER=$DOCKER_USER
 dokku git:sync admin "https://github.com/${REPO_URL}" --build
+
+# Create user specifically for direct Docker access
+echo '-----> Creating Docker user'
+adduser --disabled-password --gecos "" $DOCKER_USER
+usermod -aG docker $DOCKER_USER
+
+# Generate SSH keys for both new user and dokku
+echo '-----> Generating SSH keys'
+ssh-keygen -b 4096 -t rsa -f dokku_key -N ""
+ssh-keygen -b 4096 -t rsa -f docker_key -N ""
+
+# Add the public keys to both new user and dokku
+echo '-----> Adding SSH keys to users and dokku'
+mkdir /home/$DOCKER_USER/.ssh/
+touch /home/$DOCKER_USER/.ssh/authorized_keys
+echo $(cat docker_key.pub) >> /home/$DOCKER_USER/.ssh/authorized_keys
+cat dokku_key.pub | dokku ssh-keys:add _admin
+
+# Add private keys to config
+echo '-----> Linking SSH keys to admin'
+dokku config:set admin DOKKU_SSH_KEY="$(cat dokku_key)" DOCKER_SSH_KEY="$(cat docker_key)"
+
+# Cleanup
+echo '-----> Cleaning up...'
+unset DOCKER_USER
+unset SETUP_KEY
+unset HOST_IP
+unset CURRENT_USER
+unset CURRENT_VERSION
+rm -rf dokku_key dokku_key.pub docker_key docker_key.pub bootstrap.sh install.sh
+dokku cleanup
 
 echo '-----> Installation complete!'
 echo "-----> Go to http://${HOST_IP}:3000/setup?key=${SETUP_KEY}"
