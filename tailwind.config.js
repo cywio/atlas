6bf6dd337b9f1ab430da3fe9d8fbce6a45d5fd23
Chris Walsh <christianywalsh@gmail.com>
2022-01-10 02:08:57 -0800

    Upgrade tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-	mode: 'jit',
-	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './lib/components/**/*.{js,ts,jsx,tsx}'],
-	darkMode: false, // or 'media' or 'class'
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './lib/components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
